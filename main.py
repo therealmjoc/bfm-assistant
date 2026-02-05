136257066b139f1ff2c62fdbe9f4c2c39e8970b5
Matthew <matthewj.oconnor01@gmail.com>
2026-02-05 13:40:57 +0000

    insertShow function roughly defined

--- main.py
+++ main.py
@@ -10,3 +10,31 @@ print("""
 Pre-alpha version. CONCEPT ONLY.
 -------------------------------------
 """)
+
+# timetable structure
+monday = ['','','','','','','','','','','','']
+tuesday = ['','','','','','','','','','','','']
+wednesday = ['','','','','','','','','','','','']
+thursday = ['','','','','','','','','','']
+friday = ['','','','','','','','','','']
+
+timetable = [monday,tuesday,wednesday,thursday,friday]
+
+# show slots are defined as a tuple [int day, int timeslot],
+# where the day is a day 0-4 and timeslot is a time 0-11
+
+'''
+Arguments
+name: the name of the show
+slots: a list slots of the show, stored as a list of tuples
+
+Inserts a show into the timetable in order of the timeslots given. If their first preference is not available, it will fall
+back on their second etc.
+'''
+def insertShow(name, slots):
+    for slot in slots:
+        if timetable[slot[0]][slot[1]] == '':
+            timetable[slot[0]][slot[1]] = name
+
+insertShow("In the loop",[[0,0]])
+print(timetable)
